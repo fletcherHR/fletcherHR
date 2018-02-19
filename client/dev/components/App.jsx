6c5efb1737233e12e5cd88cc4c8763c20e9001d9
Eric Sin <eric@Erics-MacBook-Pro.local>
2018-02-19 16:20:47 -0500

    Basic set up of files and folders

---
+++ client/dev/components/App.jsx
@@ -0,0 +1,16 @@
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Fletcher Greenfield Project: Job Search?</h1>
+      </div>
+    )
+  }
+}
