442918a470fd12f53d52286f8f4f68f85c469af2
Eric Sin <eric@Erics-MacBook-Pro.local>
2018-02-20 16:06:22 -0500

    functioning dynamic list rendering from search result

--- client/dev/components/App.jsx
+++ client/dev/components/App.jsx
@@ -1,38 +1,43 @@
 import React from 'react';
 import Search from './Search.jsx';
-import GoogleMaps from './GoogleMaps.jsx'
+import GoogleMaps from './GoogleMaps.jsx';
+import ResultList from './ResultList.jsx';
 import axios from 'axios';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
-
+      resultList: [{prices: 500, addresses: 'addresses', images: 'imageurl'}],
       // default is HR right now
       latitude: 40.750611,
       longitude: -73.978641
-
     };
-
-
-
-
+    this.packData = this.packData.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  packData({ prices, addresses, images }) {
+    const temp = [];
+    for (let i = 0; i < prices.length; i += 1) {
+      const obj = { prices: prices[i], addresses: addresses[i], images: images[i] };
+      temp.push(obj);
+    }
+    this.setState({ resultList: temp }, () => console.log('this is the updated state: ', this.state));
+    console.log('second check on state: ', this.state);
   }
 
   handleSearch({ userAddress, userCommute, userRent }) {
     const userInfo = { userAddress, userCommute, userRent };
     const zip = (userInfo.userAddress.slice(userInfo.userAddress.length - 5, userInfo.userAddress.length));
-    console.log('this is zip', {zip: zip });
-    axios.post('/zillow', {zip: zip})
+    console.log('this is zip', { zip:zip });
+    axios.post('/zillow', { zip:zip })
       .then((res) => {
         // make sure we are sending back data in an array
-        console.log(res.data);
-        this.setState({ resultList: res.data })
+        this.packData(res.data);
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
       })
 
   }
@@ -43,6 +48,7 @@ export default class App extends React.Component {
         <h1>Fletcher Greenfield Project: Job Search?</h1>
 
         <Search triggerSearch={this.handleSearch} />
+        <ResultList resultList={this.state.resultList} />
         <GoogleMaps isMarkerShown
   					googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
   					loadingElement={<div style={{ height: `100%` }} />}
