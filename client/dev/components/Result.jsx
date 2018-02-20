9c9e8b7c3964f6c12939df0632ae2d7c131c3ce8
chrisr <christopher.rigoli@gmail.com>
2018-02-20 18:39:07 -0500

    still working on front end favorites functionality

--- client/dev/components/Result.jsx
+++ client/dev/components/Result.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import ReactStars from 'react-stars';
 
 export default function Result(props) {
+  console.log('these are props within Result.jsx: ', props);
+  const imageURL = props.result.images;
+  const saveToFavorites = () => {
+    // this will save to favorites
+  };
+
   return (
-    <li>{props.result.prices}{props.result.addresses}{props.result.images}</li>
+    <div>
+      <ReactStars
+        count={1}
+        onChange={saveToFavorites}
+        size={16}
+        color2={'#ffd700'}
+        half={false}
+      />
+      <span>
+        ${props.result.prices}
+      </span>
+      <span>
+        - {props.result.addresses}
+      </span>
+      <span>
+        - <a href={imageURL}>Property Image</a>
+      </span>
+    </div>
   );
 }
