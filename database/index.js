bac1d05f83e5a07642959d6dc16c5aa05f009b29
chrisr <christopher.rigoli@gmail.com>
2018-02-20 14:44:47 -0500

    updated db schema, added connection to db, added db helper functions

--- database/index.js
+++ database/index.js
@@ -1,15 +1,23 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
+  host: 'localhost',
   user: 'root',
   password: '',
-  database: 'jobsearch'
+  database: 'search',
 });
 
 connection.connect((err) => {
-  if(err) {console.log(err)}
+  if (err) {
+    console.log(`error connecting: ${err}`);
+  } else {
+    console.log(`connected as id ${connection.threadId}`);
+  }
+});
 
-  console.log('Database connected')
-})
+connection.query('SELECT * FROM users', (error, rows) => {
+  if (error) console.log('ERROR in connection.query: ', error);
+  else console.log('these are the results: ', JSON.parse(JSON.stringify(rows)));
+});
 
-module.exports = connection;
+module.exports.connection = connection;
