b6e9ab2c51d7c65d7f992c4ab3edd44baeb4bf10
Xixi Chen <31266066+xixihaha4@users.noreply.github.com>
2018-02-20 12:03:49 -0500

    incoporated google maps

--- client/dev/components/App.jsx
+++ client/dev/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Search from './Search.jsx'
+import Search from './Search.jsx';
+import GoogleMaps from './GoogleMaps.jsx'
 
 export default class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-
+      latitude:'-34.397',
+      longitude: '150.644'
     }
   }
 
@@ -14,6 +16,14 @@ export default class App extends React.Component{
       <div>
         <h1>Fletcher Greenfield Project: Job Search?</h1>
         <Search />
+        <GoogleMaps isMarkerShown
+  					googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+  					loadingElement={<div style={{ height: `100%` }} />}
+  					containerElement={<div style={{ height: `400px` }} />}
+  					mapElement={<div style={{ height: `100%` }}/>}
+  					latitude={this.state.latitude}
+  					longitude={this.state.longitude}
+  			/>
       </div>
     )
   }
