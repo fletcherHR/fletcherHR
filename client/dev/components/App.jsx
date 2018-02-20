eb7d7b463d4eb791ed22443adfc31b29780f1a4b
Eric Sin <eric@Erics-MacBook-Pro.local>
2018-02-20 14:05:28 -0500

    functioning search getting results

--- client/dev/components/App.jsx
+++ client/dev/components/App.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import Search from './Search.jsx';
+import axios from 'axios';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      resultList: []
     };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch({ userAddress, userCommute, userRent }) {
+    const userInfo = { userAddress, userCommute, userRent };
+    const zip = (userInfo.userAddress.slice(userInfo.userAddress.length - 5, userInfo.userAddress.length));
+    console.log('this is zip', {zip: zip });
+    axios.post('/zillow', {zip: zip})
+      .then((res) => {
+        // make sure we are sending back data in an array
+        console.log(res.data);
+        this.setState({ resultList: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   render() {
     return (
       <div>
         <h1>Fletcher Greenfield Project: Job Search?</h1>
-        <Search />
+        <Search triggerSearch={this.handleSearch} />
       </div>
     );
   }
